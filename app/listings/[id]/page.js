9057f09e2f1b95d5c3dda922ca7231391999c6db
Garret <blankenship.151@gmail.com>
2024-05-05 05:58:47 +0000

    created dynamic page linking

---
+++ app/listings/[id]/page.js
@@ -0,0 +1,38 @@
+import myQuery from "@/database/myQuery.mjs";
+import styles from "./styles.module.css";
+
+async function getProductByID(id){
+    const sqlSelectProduct = `SELECT *  FROM Products WHERE id=${id}`;
+    const results = await myQuery(sqlSelectProduct);
+    return results[0];
+}
+
+async function getProductImagesByID(id){
+    const sqlSelectImages = `SELECT * FROM Product_Images WHERE products_id=${id}`;
+    const results = await myQuery(sqlSelectImages);
+    return results;
+}
+
+export default async function SingleListingPage(props){
+    const product = await getProductByID(props.params.id);
+    const images = await getProductImagesByID(props.params.id);
+   
+    
+    return(
+        <div>
+            <div className={styles.lefthandColumn}>
+                <div className={styles.mainImage} style={{backgroundImage: `url(/product_images/${images[0].url})`}}></div>
+
+            </div>
+            <div className={styles.righthandColumn} >
+                <h1>{product.title}</h1>
+                <h2>Price: ${product.price/100}</h2>
+                <h2>Location: {product.location}</h2>
+                <h3>Description</h3>
+                <p>{product.description}</p>
+
+            </div>
+        </div>
+
+    )
+}
--- app/listings/page.js
+++ app/listings/page.js
@@ -1,5 +1,7 @@
 import myQuery from "@/database/myQuery.mjs";
 import styles from "./styles.module.css";
+import Link from "next/link";
+
 
 async function getProducts(){
     const sqlSelectProducts = `SELECT Products.*, Product_Images.url FROM Products INNER JOIN Product_Images ON Products.id = Product_Images.products_id WHERE Product_Images.main = true;`;
@@ -18,12 +20,14 @@ export default async function ListingsPage() {
             <h1>Listings</h1>
             {products.map((product)=>{
                 return(
-                    <div>
-                        <div className={styles.mainProductImage} style={{backgroundImage:`url(/product_images/${product.url})`}}></div>
-                        <h2>{product.title}</h2>
-                        <p>Price: ${product.price/100}</p>
-                        <p>Location: {product.location}</p>
-                    </div>
+                    <Link href={`/listings/${product.id}`}>
+                        <div>
+                            <div className={styles.mainProductImage} style={{backgroundImage:`url(/product_images/${product.url})`}}></div>
+                            <h2>{product.title}</h2>
+                            <p>Price: ${product.price/100}</p>
+                            <p>Location: {product.location}</p>
+                        </div>
+                    </Link>
                 )
             })}
       </main>
